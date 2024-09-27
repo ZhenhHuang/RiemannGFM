da3e529c29994e442dca59660c06f33591a0ffe9
ZhenhHuang <huangzhenhao_work@163.com>
2024-09-27 15:40:26 +0800

    Update

--- data/__init__.py
+++ data/__init__.py
@@ -1,10 +1,6 @@
 from .dataset_vallina import load_data, input_dim_dict, class_num_dict
-from .dataset_node import PretrainingNodeDataset, InductiveNodeClsDataset, TransductiveNodeClsDataset
-from .dataset_link import LinkPredDataset
 from .loader import ExtractNodeLoader, ExtractLinkLoader
 
 
 __all__ = ['load_data', 'input_dim_dict', 'class_num_dict',
-           'PretrainingNodeDataset', 'InductiveNodeClsDataset',
-           'LinkPredDataset', 'TransductiveNodeClsDataset', 'ExtractNodeLoader',
-           'ExtractLinkLoader']
+           'ExtractNodeLoader', 'ExtractLinkLoader']
